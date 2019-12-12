cbd9e2294d8e5f1afad792efaf6f41d609e9eab4
bycolour2 <ruskir7@gmail.com>
2019-12-12 21:30:28 +0300

    Added: -submit button -check on existing todo -some fool checks

--- angular-frontend/src/app/todo-list.component.ts
+++ angular-frontend/src/app/todo-list.component.ts
@@ -1,52 +1,59 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { TodoService } from './todo.service';
-import { Todo } from './todo';
-import {NgForm} from '@angular/forms';
+import { Component, Input, OnInit } from "@angular/core";
+import { TodoService } from "./todo.service";
+import { Todo } from "./todo";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'todo-list',
-  templateUrl: './todo-list.component.html'
+  selector: "todo-list",
+  templateUrl: "./todo-list.component.html"
 })
-
 export class TodoListComponent implements OnInit {
   todos: Todo[];
   newTodo: Todo = new Todo();
   editing: boolean = false;
   editingTodo: Todo = new Todo();
+  existingTodo: boolean = false;
 
-  constructor(
-    private todoService: TodoService,
-  ) {}
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
     this.getTodos();
   }
 
+  checkTodo(): void {
+    this.todos.some(todo => {
+      if (todo.title === this.newTodo.title) {
+        return (this.existingTodo = true);
+      } else {
+        this.existingTodo = false;
+      }
+    });
+  }
+
   getTodos(): void {
-    this.todoService.getTodos()
-      .then(todos => this.todos = todos );
+    this.todoService.getTodos().then(todos => (this.todos = todos));
   }
 
   createTodo(todoForm: NgForm): void {
-    this.todoService.createTodo(this.newTodo)
-      .then(createTodo => {
+    this.checkTodo();
+    if (this.existingTodo === false) {
+      this.todoService.createTodo(this.newTodo).then(createTodo => {
         todoForm.reset();
         this.newTodo = new Todo();
-        this.todos.unshift(createTodo)
+        this.todos.unshift(createTodo);
       });
+    }
   }
 
   deleteTodo(id: string): void {
-    this.todoService.deleteTodo(id)
-    .then(() => {
+    this.todoService.deleteTodo(id).then(() => {
       this.todos = this.todos.filter(todo => todo.id != id);
     });
   }
 
   updateTodo(todoData: Todo): void {
     console.log(todoData);
-    this.todoService.updateTodo(todoData)
-    .then(updatedTodo => {
+    this.todoService.updateTodo(todoData).then(updatedTodo => {
       let existingTodo = this.todos.find(todo => todo.id === updatedTodo.id);
       Object.assign(existingTodo, updatedTodo);
       this.clearEditing();
@@ -55,8 +62,7 @@ export class TodoListComponent implements OnInit {
 
   toggleCompleted(todoData: Todo): void {
     todoData.completed = !todoData.completed;
-    this.todoService.updateTodo(todoData)
-    .then(updatedTodo => {
+    this.todoService.updateTodo(todoData).then(updatedTodo => {
       let existingTodo = this.todos.find(todo => todo.id === updatedTodo.id);
       Object.assign(existingTodo, updatedTodo);
     });
